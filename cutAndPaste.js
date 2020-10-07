59b46d994434b0c6f49873dcf01bcde50f2b0775
James Mernin <jamespmernin@gmail.com>
2020-10-07 12:57:51 -0400

    James Mernin algorithm solved

--- cutAndPaste.js
+++ cutAndPaste.js
@@ -1,7 +1,10 @@
 //  CUT AND PASTE - 
 
 const cutAndPaste = (arr, num) => {
-
+  for (let i = 0; i < num; i++) {
+    arr.push(arr.shift());
+  }
+  return arr;
 }
 
 console.log(cutAndPaste([1, 2, 5, "help", "wow", 3], 1))
